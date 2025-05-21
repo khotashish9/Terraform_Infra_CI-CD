e902d56862c2705dedf3ec1938fbd41763bbffce
Ashish Khot <khotashish9@gmail.com>
2025-05-21 14:17:54 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstatestorage123"
+    resource_group_name  = "csi-rg-test"
+    storage_account_name = "tfstatestraccount"
     container_name       = "tfstate"
     key                  = "landingzone.terraform.tfstate"
   }
