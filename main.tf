424c2fbe9373bdf7fbcad3d9bcd7cd6b7066eca9
Ashish Khot <khotashish9@gmail.com>
2025-05-22 12:11:53 +0530

    update

--- main.tf
+++ main.tf
@@ -40,25 +40,6 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-win-vm"
-  location = "East US"
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-win"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-win"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
 resource "azurerm_network_interface" "nic" {
   name                = "nic-win"
   location            = azurerm_resource_group.rg.location
